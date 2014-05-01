647e46e23369359c4dd86324164177aa50138be1
Beth Watson <bwatson2@binghamton.edu>
2014-05-01 15:55:26 -0300

    validations in model; passing validation specs; up/down migration

--- db/migrate/20140501181258_create_users.rb
+++ db/migrate/20140501181258_create_users.rb
@@ -1,5 +1,5 @@
 class CreateUsers < ActiveRecord::Migration
-  def change
+  def up
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
@@ -11,4 +11,8 @@ class CreateUsers < ActiveRecord::Migration
       t.timestamps
     end
   end
+
+  def down
+	droptable :users
+   end
 end
