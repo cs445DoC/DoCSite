2abb93cfb4c716671d48e702095cdd4baaee9a3a
Beth Watson <bwatson2@binghamton.edu>
2014-05-15 02:07:25 -0300

    persists fuel usage

--- app/controllers/fuel_usages_controller.rb
+++ app/controllers/fuel_usages_controller.rb
@@ -5,6 +5,12 @@ class FuelUsagesController < ApplicationController
   end
 
   def create
+    m = FuelUsage.new
+    m.date = params[:fuel_usage]['date(1i)'] + '-' + params[:fuel_usage]['date(2i)'] + '-' + params[:fuel_usage]['date(3i)']
+    m.mileage = params[:fuel_usage][:mileage]
+    m.gallons = params[:fuel_usage][:gallons]
+    m.license_plate = params[:fuel_usage][:license_plate]
+    m.save!
     render 'users/show'
   end
 
--- app/models/fuel_usage.rb
+++ app/models/fuel_usage.rb
@@ -1,6 +1,6 @@
 class FuelUsage < ActiveRecord::Base
 
-   attr_accessor :date, :gallons, :license_plate, :mileage
+
    attr_accessible :date, :gallons, :license_plate, :mileage
 
    validates :date, presence: true
