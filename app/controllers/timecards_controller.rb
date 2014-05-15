b45e7ba7ca2e0d21263190aa38a26801d0c10f05
saasbook <saasbook@saasbook.(none)>
2014-05-15 01:01:55 -0400

    Timecard fixes.

--- app/controllers/timecards_controller.rb
+++ app/controllers/timecards_controller.rb
@@ -1,9 +1,14 @@
 class TimecardsController < ApplicationController
   def new
-    @timecard = Timecard.new
   end
 
   def create
+    t = Timecard.new
+    t.weekending = params[:timecard]['weekending(1i)'] + '-' + params[:timecard]['weekending(2i)'] + '-' + params[:timecard]['weekending(3i)']
+    t.date = params[:timecard]['date(1i)'] + '-' + params[:timecard]['date(2i)'] + '-' + params[:timecard]['date(3i)']
+    t.hours_worked = params[:timecard][:hours_worked]
+    t.paycode = params[:timecard][:paycode]
+    t.save!
     render 'users/show'
   end
 
